23995e7ddc877210a89d06267d7a4161a47077b6
mda01 <mage.de.lombre@gmail.com>
2025-09-21 01:31:14 +0200

    feat: created main files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let puzzle_name = "test_puzzles/puzzle00.txt";
+    let puzzle_str = fs::read_to_string(puzzle_name).expect("Could not read file");
+    let mut puzzle_in = puzzle_str.splitn(2, '\n');
+    let n = puzzle_in.next().expect("Empty file?");
+    println!("N = {n}");
+    let puzzle = puzzle_in.next().expect("Nothing after the size?");
+    println!("{puzzle}");
 }
